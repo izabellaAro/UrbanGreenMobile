5f8d578b289ca5a8e5b9afcda5236c3598075bc5
Izabella Aro <izabella.aro20@gmail.com>
2024-08-09 12:08:12 -0300

    iniciando projeto

---
+++ app/src/main/java/com/example/urbangreenmobile/telaPrincipal.java
@@ -0,0 +1,31 @@
+package com.example.urbangreenmobile;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class telaPrincipal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.tela_principal);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void sair(View view) {
+        Intent in = new Intent(telaPrincipal.this, login.class);
+        startActivity(in);
+    }
+}
