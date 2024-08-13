e6b4eb158a1fb6c70458abd82e1ed75c62d7c77f
Izabella Aro <izabella.aro20@gmail.com>
2024-08-13 20:24:12 -0300

    Adiciona menu lateral e suas opcoes

--- app/src/main/java/com/example/urbangreenmobile/ui/TelaPrincipal.java
+++ app/src/main/java/com/example/urbangreenmobile/ui/TelaPrincipal.java
@@ -9,20 +9,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.urbangreenmobile.R;
 
 public class TelaPrincipal extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.tela_principal);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        findViewById(R.id.btnMenu).setOnClickListener(v -> {
+            drawerLayout.openDrawer(findViewById(R.id.nav_view));
         });
     }
 
